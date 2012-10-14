1d4d5c1f3f622238b8af9addb311de4174d5f0f1
Dave Brown <pair+dave@howaboutwe.com>
2012-10-13 20:29:51 -0400

    Changed default webservice endpoint to be S6.

--- lib/exact_target_sdk/config.rb
+++ lib/exact_target_sdk/config.rb
@@ -3,7 +3,7 @@ require 'logger'
 module ExactTargetSDK
 
   DEFAULT_TIMEOUT = 15
-  DEFAULT_ENDPOINT = 'https://webservice.s4.exacttarget.com/Service.asmx'
+  DEFAULT_ENDPOINT = 'https://webservice.s6.exacttarget.com/Service.asmx'
   DEFAULT_NAMESPACE = 'http://exacttarget.com/wsdl/partnerAPI'
 
   # Globally configures and retrieves configuration for the ExactTarget SDK.
