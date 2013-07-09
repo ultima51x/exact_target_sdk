1eb10ede58c146f7e2da21f575d08a8783142af0
Dave Brown <pair+dave@howaboutwe.com>
2013-07-09 15:27:38 -0400

    Added business unit Client to top level TriggeredSend class.

--- lib/exact_target_sdk/triggered_send.rb
+++ lib/exact_target_sdk/triggered_send.rb
@@ -4,6 +4,7 @@ class TriggeredSend < APIObject
   array_property 'Attributes'
   array_property 'Subscribers'
   property 'TriggeredSendDefinition', :required => true
+  property 'Client'
 
 end
 end
